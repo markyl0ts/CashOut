7c326fe4f306d7270d141f733546c334c6aa54bc
markyl0ts <dev.marku@yahoo.com>
2023-04-23 01:59:33 +0800

    Add test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CashOut.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok(new { code = HttpStatusCode.OK, data = default(object), message = "Test success" });
+        }
+    }
+}
